a88381f8f5b60ea32bd2db75b5d01662e15ccdd6
Nicolas Martyanoff <nicolas.martyanoff@exograd.com>
2022-02-08 19:37:44 +0100

    remove unused import

--- eventline/pipeline.py
+++ eventline/pipeline.py
@@ -12,7 +12,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
 # IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from typing import Any, Optional, Dict
+from typing import Any, Dict
 
 from eventline.api_object import ReadableAPIObject
 
